26cf6c9b891f88f39692a276f47105f3b3fb0a04
Chonlasith Jucksriporn <chonlasith@gmail.com>
2024-10-25 19:55:58 +0700

    chore: get env by os.environ

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ for feed_item in feed["rss"]["channel"]["item"]:
 
 # Get my repositories
 print("Collecting repositories data...")
-GITHUB_TOKEN = os.getenv("GH_TOKEN")
+GITHUB_TOKEN = os.environ["GH_TOKEN"]
 print(GITHUB_TOKEN[:5])
 headers = {
     "Accept": "application/vnd.github+json",
